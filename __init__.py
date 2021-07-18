90f315fbf4df2273206adfc3d4b17b817b459d7a
lucianojamimbo <65134690+LJ3D@users.noreply.github.com>
2021-07-18 20:23:36 +0100

    Support for alpha + spec textures
    Also fixed a mistake where node_imTexDiffuse was being renamed to "node_imTexMetallic"
    
    Also other minor stuff like merging bumpNames into dispNames and glossNames into roughNames

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "AssetLibraryTools",
     "description": "Set of tools to speed up the creation of asset libraries for the asset browser introduced in blender 3.0",
     "author": "Lucian James (LJ3D)",
-    "version": (0, 0, 5),
+    "version": (0, 0, 6),
     "blender": (3, 0, 0),
     "location": "3D View > Tools",
     "warning": "", # used for warning icon and text in addons panel
@@ -35,14 +35,14 @@ import os
 # ------------------------------------------------------------------------ 
 
 diffNames = ["diffuse", "diff", "albedo", "base", "col", "color"]
-sssNames = ["sss", "subsurface"]
+sssNames = ["sss", "subsurface"] #todo
 metNames = ["metallic", "metalness", "metal", "mtl"]
 specNames = ["specularity", "specular", "spec", "spc"]
-roughNames = ["roughness", "rough", "rgh"]
-glossNames = ["gloss", "glossy", "glossiness"]
+roughNames = ["roughness", "rough", "rgh", "gloss", "glossy", "glossiness"]
 normNames = ["normal", "nor", "nrm", "nrml norm"]
-bumpNames = ["bump", "bmp"]
-dispNames = ["displacement", "displace", "disp", "dsp", "height", "heightmap"]
+dispNames = ["displacement", "displace", "disp", "dsp", "height", "heightmap", "bump", "bmp"]
+alphaNames = ["alpha", "opacity"]
+emissiveNames = ["emissive", "emission"] #todo
 
 # Function partly stolen from node wrangler :D
 def FindPBRTextureType(fname):
@@ -72,14 +72,14 @@ def FindPBRTextureType(fname):
             PBRTT = "spec"
         if i in roughNames:
             PBRTT = "rough"
-        if i in glossNames:
-            PBRTT = "gloss"
         if i in normNames:
             PBRTT = "norm"
-        if i in bumpNames:
-            PBRTT = "bump"
         if i in dispNames:
             PBRTT = "disp"
+        if i in alphaNames:
+            PBRTT = "alpha"
+        if i in emissiveNames:
+            PBRTT = "emission"
     return PBRTT
 
 def DisplayMessageBox(message = "", title = "Info", icon = 'INFO'):
@@ -131,19 +131,25 @@ class createPBR():
 
         # Create texture nodes
         node_imTexDiffuse = nodes.new(type="ShaderNodeTexImage")
-        node_imTexDiffuse.location = -800,300
+        node_imTexDiffuse.location = -800,600
         node_imTexDiffuse.name = "node_imTexDiffuse"
         node_imTexMetallic = nodes.new(type="ShaderNodeTexImage")
-        node_imTexMetallic.location = -800,0
-        node_imTexDiffuse.name = "node_imTexMetallic"
+        node_imTexMetallic.location = -800,300
+        node_imTexMetallic.name = "node_imTexMetallic"
+        node_imTexSpecular = nodes.new(type="ShaderNodeTexImage")
+        node_imTexSpecular.location = -800,0
+        node_imTexSpecular.name = "node_imTexSpecular"
         node_imTexRoughness = nodes.new(type="ShaderNodeTexImage")
         node_imTexRoughness.location = -800,-300
         node_imTexRoughness.name = "node_imTexRoughness"
+        node_imTexAlpha = nodes.new(type="ShaderNodeTexImage")
+        node_imTexAlpha.location = -800, -600
+        node_imTexAlpha.name = "node_imTexAlpha"
         node_imTexNormal = nodes.new(type="ShaderNodeTexImage")
-        node_imTexNormal.location = -800,-600
+        node_imTexNormal.location = -800,-900
         node_imTexNormal.name = "node_imTexNormal"
         node_imTexDisplacement = nodes.new(type="ShaderNodeTexImage")
-        node_imTexDisplacement.location = -800,-900
+        node_imTexDisplacement.location = -800,-1200
         node_imTexDisplacement.name = "node_imTexDisplacement"
         
         # Create norm+disp nodes
@@ -156,20 +162,28 @@ class createPBR():
         
         # Load textures
         diffuseTexture = None
+        specularTexture = None
         metallicTexture = None
         roughnessTexture = None
+        alphaTexture = None
         normalTexture = None
         displacementTexture = None
         for i in files:
             t = FindPBRTextureType(i.name)
             if t == "diff":
                 diffuseTexture = bpy.data.images.load(str(i))
+            elif t == "spec":
+                specularTexture = bpy.data.images.load(str(i))
+                metallicTexture.colorspace_settings.name = 'Non-Color'
             elif t == "met":
                 metallicTexture = bpy.data.images.load(str(i))
                 metallicTexture.colorspace_settings.name = 'Non-Color'
             elif t == "rough":
                 roughnessTexture = bpy.data.images.load(str(i))
                 roughnessTexture.colorspace_settings.name = 'Non-Color'
+            elif t == "alpha":
+                alphaTexture = bpy.data.images.load(str(i))
+                alphaTexture.colorspace_settings.name = 'Non-Color'
             elif t == "norm":
                 normalTexture = bpy.data.images.load(str(i))
                 normalTexture.colorspace_settings.name = 'Non-Color'
@@ -187,6 +201,13 @@ class createPBR():
             links.new(node_mapping.outputs[0], node_imTexDiffuse.inputs[0])
         else:
             nodes.remove(node_imTexDiffuse)
+           
+        if specularTexture != None and tool.import_spec != False:
+            node_imTexSpecular.image = specularTexture
+            links.new(node_imTexSpecular.outputs[0], node_principled.inputs[5])
+            links.new(node_mapping.outputs[0], node_imTexSpecular.inputs[0])
+        else:
+            nodes.remove(node_imTexSpecular)
             
         if metallicTexture != None and tool.import_met != False:
             node_imTexMetallic.image = metallicTexture
@@ -201,6 +222,13 @@ class createPBR():
             links.new(node_mapping.outputs[0], node_imTexRoughness.inputs[0])
         else:
             nodes.remove(node_imTexRoughness)
+            
+        if alphaTexture != None and tool.import_alpha != False:
+            node_imTexAlpha.image = alphaTexture
+            links.new(node_imTexAlpha.outputs[0], node_principled.inputs[19])
+            links.new(node_mapping.outputs[0], node_imTexAlpha.inputs[0])
+        else:
+            nodes.remove(node_imTexAlpha)
             
         if normalTexture != None and tool.import_norm != False:
             node_imTexNormal.image = normalTexture
@@ -266,10 +294,20 @@ class properties(PropertyGroup):
         description = "",
         default = True
         )
+    import_spec : BoolProperty(
+        name = "Import specularity",
+        description = "",
+        default = False
+        )   
     import_rough : BoolProperty(
         name = "Import roughness",
         description = "",
         default = True
+        )
+    import_alpha : BoolProperty(
+        name = "Import alpha",
+        description = "",
+        default = False
         )
     import_norm : BoolProperty(
         name = "Import normal",
@@ -279,7 +317,7 @@ class properties(PropertyGroup):
     import_disp : BoolProperty(
         name = "Import displacement",
         description = "",
-        default = True
+        default = False
         )
     
     # Model import properties
@@ -624,7 +662,9 @@ class OBJECT_PT_panel(Panel):
                 matImportBox.label(text="Import following textures into materials (if found):")
                 matImportBox.prop(tool, "import_diff")
                 matImportBox.prop(tool, "import_met")
+                matImportBox.prop(tool, "import_spec")
                 matImportBox.prop(tool, "import_rough")
+                matImportBox.prop(tool, "import_alpha")
                 matImportBox.prop(tool, "import_norm")
                 matImportBox.prop(tool, "import_disp")
         
@@ -704,6 +744,7 @@ class OBJECT_PT_panel(Panel):
         if obj.assetDownloaderRow_expanded:
             assetDownloaderRow = assetDownloaderBox.row()
             assetDownloaderBox.prop(tool, "downloader_save_path")
+            assetDownloaderBox.label(text='Make sure to uncheck "Relative Path"!')
             assetDownloaderBox.prop(tool, "keywordFilter")
             assetDownloaderBox.prop(tool, "attributeFilter")
             assetDownloaderBox.prop(tool, "extensionFilter")
